22762e54557050d33e9cb9cecb63917a87b978ba
Nehal Ahmed <nehal.ahmed@tallysolutions.com>
2025-05-24 22:36:34 +0530

    file updates

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,3 +1,12 @@
+// import axios from 'axios';
+// export const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+// export const fetchCategories = () => API.get('/categories');
+
 import axios from 'axios';
-export const API = axios.create({ baseURL: 'https://e-backend-rf04.onrender.com/api' });
+
+export const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+});
+
+
 export const fetchCategories = () => API.get('/categories');
