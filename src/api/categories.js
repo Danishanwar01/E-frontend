3f7fed9bcac94bc4300413749eb2d33f756c0122
Nehal Ahmed <nehal.ahmed@tallysolutions.com>
2025-05-25 11:05:55 +0530

    Add slick-carousel to dependencies

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,12 +1,12 @@
-// import axios from 'axios';
-// export const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-// export const fetchCategories = () => API.get('/categories');
-
 import axios from 'axios';
-
-export const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
-});
-
-
+export const API = axios.create({ baseURL: 'https://e-backend-rf04.onrender.com/api' });
 export const fetchCategories = () => API.get('/categories');
+
+// import axios from 'axios';
+
+// export const API = axios.create({
+//   baseURL: process.env.REACT_APP_API_BASE_URL
+// });
+
+
+// export const fetchCategories = () => API.get('/categories');
